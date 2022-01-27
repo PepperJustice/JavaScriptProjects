81becb05ca85dbd3bd8e95b05bcfdf109391b743
Carla Justice <carla.n.justice@gmail.com>
2022-01-27 13:07:34 -0800

    Step 64  Basic Javascript
    All checked except fontcolor, see comments in js code

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("This is Fun!");//displays a pop up  window alert with this text
+var Yoga = "Ashtanga", intensity = "high", sweat = "sweaty", length = "2 hours", level = "expert";//created a multiple string variable
+
+document.write(level);//display level variable
+
+document.write(3 + 3);//expression, display value
+
+document.write("Sometimes you eat the bear," 
++"sometimes the bear eats you!"
++"You just don\'t know which it will be.");//concatenated 
+
+var A = "I need" + "coffee"//concatenated sting value
+document.write(A);//display value
+
+var B = "NOW!"//string value to variable
+document.write(A+B);
+
+//spent way too much time looking up how to change font color. very complex answers and W3 says it is deprecated to avoid
