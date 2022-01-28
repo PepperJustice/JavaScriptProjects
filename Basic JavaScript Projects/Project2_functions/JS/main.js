8eb783e6ee0485a9c24697210e9e9abd406dddd7
Carla Justice <carla.n.justice@gmail.com>
2022-01-28 08:26:14 -0800

    Project2_Functions
    Step 69

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "This is the button Text!";
+    document.getElementById("button_text").innerHTML =str;
+}
+
+function myFunction() {
+    var sentence ="I am learning";
+    sentence += "a lot from the book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
