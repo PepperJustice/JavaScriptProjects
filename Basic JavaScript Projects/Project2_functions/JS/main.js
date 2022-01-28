ba4ff080ef6ae7ac28d12c9f3a3b8f75f314225b
Carla Justice <carla.n.justice@gmail.com>
2022-01-28 08:29:26 -0800

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
 function My_First_Function() {
-    var str = "This is the button Text!";
-    document.getElementById("button_text").innerHTML =str;
+    var str = "This is the button Text!";//create a button
+    document.getElementById("button_text").innerHTML =str;//get by id of button text
 }
 
 function myFunction() {
-    var sentence ="I am learning";
-    sentence += "a lot from the book!";
-    document.getElementById("concatenate").innerHTML = sentence;
+    var sentence ="I am learning";//first part of sentence to display
+    sentence += "a lot from the book!";//second part
+    document.getElementById("concatenate").innerHTML = sentence;//will go together once clicked on and display both together
 }
